f8c5d62f7b17f33f32ed30101046fbd8ecd1464f
James Mwangi <karokijames40@gmail.com>
2015-12-13 13:37:16 +0300

    Update expert dashboard

--- resources/views/partials/innovations/innovation.blade.php
+++ resources/views/partials/innovations/innovation.blade.php
@@ -12,7 +12,7 @@
         <h3 class="inno-title">{{ $innovation->innovationTitle }}</h3>
 
         @if(\Auth::user()->id  == $innovation->user_id)
-        <h4 class="inno-innovator">by <a href="#">you</a></h4>
+        <h4 class="inno-innovator">by <a href="{{ url('innovator/profile/'.$innovation->user_id) }}">Me</a></h4>
         @else
         <h4 class="inno-innovator">by <a href="{{ url('innovator/profile/'.$innovation->user_id) }}">{{ $innovation->user->first_name }} {{ $innovation->user->last_name }}</a></h4>
         @endif
