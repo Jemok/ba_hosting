2938aa87d63e11a1b5895f39e4a740d9e1c6fee3
Michael Mwaura <mail@michaelmwaura.co.ke>
2015-12-21 20:57:30 +0300

    Fix how mother sees requests

--- resources/views/request/bongo/all.blade.php
+++ resources/views/request/bongo/all.blade.php
@@ -1,27 +1,48 @@
 @extends('layout')
 
 @section('content')
-<h4>Bongo employee requests</h4>
-@foreach($requests as $request)
+<div class="container">
+    @if(Session::has('flash_message'))
 
+    <div class="alert-message alert alert-success {{ Session::has('flash_message_important') ? 'alert-important' : '' }}">
+        @if(Session::has('flash_message_important'))
 
-{{ $request->bongo_email }}
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
-@if($request->request_status == 0)
+        @endif
 
-<a href="{{ url('request/bongo-employee/send/'.$request->id) }}"><button>Send link</button></a>
+        {{ session('flash_message') }}
 
-@elseif($request->request_status == 1)
+    </div>
 
-<button>Link sent </button>
-@elseif($request->request_status == 2)
+    @endif
 
-<button>Registered</button>
-@endif
+    <section class="requests">
+        <h2 class="section__title">Bongo employee requests</h2>
+        <div class="card-columns">
+            @foreach($requests as $request)
+            
+            <div class="card card-block request-card">
+                <h4 class="request__name">Lorem Ipsum</h4>
+                <p class="card-text">
+                    <span class="request__company"><i class="ion-briefcase"></i> Company Name</span>
+                    <span class="request__job-title"><i class="ion-pound"></i> Job title</span>
+                    <span class="request__email"><i class="ion-at"></i> {{ $request->bongo_email }}</span>
+                </p>
+                
+                @if($request->request_status == 0)
+                <a class="btn btn-primary" href="{{ url('request/bongo-employee/send/'.$request->id) }}">Send link</a>
+                @elseif($request->request_status == 1)
+                <button class="btn btn-primary">Link sent </button>
+                @elseif($request->request_status == 2)
+                <button class="btn btn-primary">Registered</button>
+                @endif
+            </div>
 
-<br>
-
-@endforeach
+            @endforeach
+        </div>
+    </section>
+</div>
 @stop
 
 
--- resources/views/request/investor/all.blade.php
+++ resources/views/request/investor/all.blade.php
@@ -17,29 +17,32 @@
     </div>
 
     @endif
+
+    <section class="requests">
+        <h2 class="section__title">Investor requests</h2>
+        <div class="card-columns">
+            @foreach($requests as $request)
+
+            <div class="card card-block request-card">
+                <h4 class="request__name">Lorem Ipsum</h4>
+                <p class="card-text">
+                    <span class="request__company"><i class="ion-briefcase"></i> Company Name</span>
+                    <span class="request__job-title"><i class="ion-pound"></i> Job title</span>
+                    <span class="request__email"><i class="ion-at"></i> {{ $request->investor_email }}</span>
+                </p>
+                @if($request->request_status == 0)
+                <a class="btn btn-primary" href="{{ url('request/bongo/send/'.$request->id) }}">Send link</a>
+                @elseif($request->request_status == 1)
+                <button class="btn btn-primary">Link sent </button>
+                @elseif($request->request_status == 2)
+                <button class="btn btn-primary">Registered</button>
+                @endif
+            </div>
+
+            @endforeach
+        </div>
+    </section>
 </div>
-
-<h4>Investor requests</h4>
-@foreach($requests as $request)
-
-
-{{ $request->investor_email }}
-
-@if($request->request_status == 0)
-
-<a href="{{ url('request/bongo/send/'.$request->id) }}"><button>Send link</button></a>
-
-@elseif($request->request_status == 1)
-
-<button>Link sent </button>
-@elseif($request->request_status == 2)
-
-<button>Registered</button>
-@endif
-
-<br>
-
-@endforeach
 @stop
 
 
