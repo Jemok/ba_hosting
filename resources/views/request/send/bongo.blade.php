0a5af3ea170d0f435f4a2c9546b6a16928ac52c5
Michael Mwaura <mail@michaelmwaura.co.ke>
2015-12-22 23:22:02 +0300

    Fix error display on auth pages

--- resources/views/request/send/bongo.blade.php
+++ resources/views/request/send/bongo.blade.php
@@ -22,33 +22,41 @@
                 {!! csrf_field() !!}
                 
                 <h3 class="form__heading">Request Invitation</h3>
-                <div class="form-group">
-                    <div class="form_field {{ $errors->has('company') ? 'has-error' : ''}}" >
-                        <label class="sr-only">Company Name</label>
-                        <input type="text" name="company" class="form-control" placeholder="Your company name" />
+                <fieldset class="form__cluster">
+                    <div class="form-group">
+                        <div class="form__field {{ $errors->has('company') ? 'has-error' : ''}}" >
+                            <label class="sr-only">Company Name</label>
+                            {!! $errors->first('company', '<label class="help-block">:message</label>' ) !!}
+                            <input type="text" name="company" class="form-control" placeholder="Company name" />
+                        </div>
+
+                        <div class="form__field {{ $errors->has('job_title') ? 'has-error' : ''}}" >
+                            <label class="sr-only">Job Title</label>
+                            {!! $errors->first('job_title', '<label class="help-block">:message</label>' ) !!}
+                            <input type="text" name="job_title" class="form-control" placeholder="Job title" />
+                        </div>
                     </div>
-                    {!! $errors->first('company', '<span class="help-block">:message</span>' ) !!}
-
-                    <div class="form_field {{ $errors->has('job_title') ? 'has-error' : ''}}" >
-                        <label class="sr-only">Job Title</label>
-                        <input type="text" name="job_title" class="form-control" placeholder="Job title" />
+                    
+                    <div class="form-group">
+                        <div class="form__field {{ $errors->has('field') ? 'has-error' : ''}}" >
+                            <label class="sr-only">Expertise</label>
+                            {!! $errors->first('field', '<label class="help-block">:message</label>' ) !!}
+                            <input type="text" name="field" class="form-control" placeholder="What's your field of expertise" />
+                        </div>
                     </div>
-                    {!! $errors->first('job_title', '<span class="help-block">:message</span>' ) !!}
-                </div>
-
-                <div class="form_field {{ $errors->has('field') ? 'has-error' : ''}}" >
-                    <label class="sr-only">Expertise</label>
-                    <input type="text" name="field" class="form-control" placeholder="Your field" />
-                </div>
-                {!! $errors->first('field', '<span class="help-block">:message</span>' ) !!}
-
-                <p class="form_field">
-                    <label class="sr-only"></label>
-                    <input type="email" name="bongo_email" class="form-control" placeholder="Please enter a valid email address" />
-                </p>
-                {!! $errors->first('bongo_email', '<span class="help-block">:message</span>' ) !!}
-
-                <button type="submit" class="btn btn-primary">Submit</button>
+                       
+                    <div class="form-group">
+                        <div class="form__field {{ $errors->has('bongo_email') ? 'has-error' : ''}}" >
+                            <label class="sr-only"></label>
+                            {!! $errors->first('bongo_email', '<label class="help-block">:message</label>' ) !!}
+                            <input type="email" name="bongo_email" class="form-control" placeholder="Please enter a valid email address" />
+                        </div>
+                    </div>
+                </fieldset>
+                
+                <footer class="form__footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </footer>
             </form>
         </div>
     </section>
